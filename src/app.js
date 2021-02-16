b47328e3f868fdf05498e470e41f98ff5ba7b419
Harsh1388 <harshsahil89@gmail.cm>
2021-02-16 22:32:45 +0530

    adding back button

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get("/weather",(req,res)=>{
 
 app.get("*",(req,res)=>{
     res.render("404error",{
-        errorMsg:'Oops! page not Found'
+        errorMsg:'Oops! page not Found, Click here to go back'
     });
 });
 app.listen(port,()=>{
