a4f4f19e1f57f94b7c9dbc5bf55b9a35b7563a2d
Harsh1388 <harshsahil89@gmail.cm>
2021-02-16 22:25:05 +0530

    nodejs project

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const { static } = require("express");
+const express=require("express");
+const app=express();
+const path=require("path");
+const hbs=require("hbs");
+ 
+const port=8123; //process.env.Port || 81
+
+
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,"../templates/views"))
+hbs.registerPartials(path.join(__dirname,"../templates/partials"));
+
+//public static path
+app.use(express.static(path.join(__dirname,"../public")));
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+
+app.get("/weather",(req,res)=>{
+    res.render("weather");
+});
+
+app.get("*",(req,res)=>{
+    res.render("404error",{
+        errorMsg:'Oops! page not Found'
+    });
+});
+app.listen(port,()=>{
+    console.log("Listening to port 8123");
+});
